3e6525a5c4b41d3696166a0f2411bf3105479ff7
Dennis.B <Baust.Dennis@gmail.com>
2024-10-30 14:05:32 +0100

    accordion exercise

--- Exercise/02-accordion/src/components/Accordion.js
+++ Exercise/02-accordion/src/components/Accordion.js
@@ -1,16 +1,38 @@
+import { useState } from 'react';
 import AccordionItem from './AccordionItem';
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((item, index) => (
         <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
           key={index}
           num={index}
           title={item.title}
           text={item.text}
-        />
+        >
+          {item.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        key="test 1"
+        num={22}
+        title="Test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
--- Exercise/02-accordion/src/components/AccordionItem.js
+++ Exercise/02-accordion/src/components/AccordionItem.js
@@ -1,10 +1,8 @@
-import { useState } from 'react';
-
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen((open) => !open);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -15,7 +13,7 @@ function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? '-' : '+'}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
