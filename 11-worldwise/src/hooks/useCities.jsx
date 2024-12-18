ca3544743a6fad575c65d219b9c3b452a610f74b
Dennis.B <Baust.Dennis@gmail.com>
2024-12-18 09:15:40 +0100

    authentication

--- 11-worldwise/src/hooks/useCities.jsx
+++ 11-worldwise/src/hooks/useCities.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { CitiesContext } from '../contexts/CitiesProvider';
+import { CitiesContext } from '../contexts/CitiesContext';
 
 function useCities() {
   const context = useContext(CitiesContext);
--- 11-worldwise/src/pages/Login.jsx
+++ 11-worldwise/src/pages/Login.jsx
@@ -1,5 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
 import PageNav from '../components/PageNav';
+import { useAuth } from '../hooks/useAuth';
 import styles from './Login.module.css';
 
 export default function Login() {
@@ -7,32 +10,36 @@ export default function Login() {
   const [email, setEmail] = useState('jack@example.com');
   const [password, setPassword] = useState('qwerty');
 
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) navigate('/app', { replace: true });
+  }, [isAuthenticated, navigate]);
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
+          <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
         </div>
 
         <div className={styles.row}>
           <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
+          <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} value={password} />
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary" onClick={handleSubmit}>
+            Login
+          </Button>
         </div>
       </form>
     </main>
